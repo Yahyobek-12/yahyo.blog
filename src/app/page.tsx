a6b07ffee032775409b49a9b151681a04538f3cb
Yahyobek <caisophiya@gmail.com>
2025-07-26 21:48:03 +0500

    it's finally worked

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -5,7 +5,7 @@ const Page = () => {
     <div className='flex items-center justify-center h-screen'>
       <div>
         <h1 className='text-3xl font-bold'>Hi</h1>
-        <h2 className='text-9xl'>I'm Yakhyo</h2>
+        <h2 className='text-9xl'>I&apos;m Yakhyo</h2>
         <h3 className='text-2xl text-blue-600'>Frontend Web Developer</h3>
       </div>
     </div>
