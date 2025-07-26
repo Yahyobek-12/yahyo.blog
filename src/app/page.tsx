e0b6507d3fade312c6ee26c102297fab27381e0d
Yahyobek <caisophiya@gmail.com>
2025-07-26 21:45:55 +0500

    why is not working

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -6,7 +6,7 @@ const Page = () => {
       <div>
         <h1 className='text-3xl font-bold'>Hi</h1>
         <h2 className='text-9xl'>I'm Yakhyo</h2>
-        <h3 className='text-2xl'>Frontend Web Developer</h3>
+        <h3 className='text-2xl text-blue-600'>Frontend Web Developer</h3>
       </div>
     </div>
   )
