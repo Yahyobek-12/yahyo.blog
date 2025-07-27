d221df4c25dd999b017a2dc44444ea9cd42f2275
Yahyobek <caisophiya@gmail.com>
2025-07-27 21:24:22 +0500

    added some words

--- src/components/cursor.tsx
+++ src/components/cursor.tsx
@@ -3,53 +3,44 @@
 import { useEffect, useState } from "react";
 
 export default function Cursor() {
-    const [position, setPosition] = useState({ x: 0, y: 0 });
-    const [isClicked, setIsClicked] = useState(false);
+  const [position, setPosition] = useState({ x: 0, y: 0 });
+  const [isClicked, setIsClicked] = useState(false);
 
-    useEffect(() => {
-        const move = (e: MouseEvent) => {
-            setPosition({ x: e.clientX, y: e.clientY });
-        };
-        window.addEventListener("mousemove", move);
-        return () => window.removeEventListener("mousemove", move);
-    }, []);
+  useEffect(() => {
+    const move = (e: MouseEvent) => {
+      setPosition({ x: e.clientX, y: e.clientY });
+    };
 
-    useEffect(() => {
-        const move = (e: MouseEvent) => {
-            setPosition({ x: e.clientX, y: e.clientY });
-        };
+    const down = () => setIsClicked(true);
+    const up = () => setIsClicked(false);
 
-        const down = () => setIsClicked(true);
-        const up = () => setIsClicked(false);
+    window.addEventListener("mousemove", move);
+    window.addEventListener("mousedown", down);
+    window.addEventListener("mouseup", up);
 
-        window.addEventListener("mousemove", move);
-        window.addEventListener("mousedown", down);
-        window.addEventListener("mouseup", up);
+    return () => {
+      window.removeEventListener("mousemove", move);
+      window.removeEventListener("mousedown", down);
+      window.removeEventListener("mouseup", up);
+    };
+  }, []);
 
-        return () => {
-            window.removeEventListener("mousemove", move);
-            window.removeEventListener("mousedown", down);
-            window.removeEventListener("mouseup", up);
-        };
-    }, []);
-
-
-    return (
-        <>
-            {/* Outer cursor */}
-            <div
-                className="fixed top-0 left-0 w-12 h-12 rounded-full border border-black pointer-events-none z-[9999] transition-transform duration-150 ease-out"
-                style={{
-                    transform: `translate(${position.x - 24}px, ${position.y - 24}px)`,
-                }}
-            />
-            {/* Inner cursor */}
-            <div
-                className="fixed top-0 left-0 w-3 h-3 rounded-full bg-black pointer-events-none z-[9999] transition-transform duration-75 ease-out"
-                style={{
-                    transform: `translate(${position.x - 6}px, ${position.y - 6}px)`,
-                }}
-            />
-        </>
-    );
+  return (
+    <>
+      {/* Outer cursor */}
+      <div
+        className="fixed top-0 left-0 w-12 h-12 rounded-full border border-black pointer-events-none z-[9999] transition-transform duration-150 ease-out"
+        style={{
+          transform: `translate(${position.x - 24}px, ${position.y - 24}px) scale(${isClicked ? 0.7 : 1})`,
+        }}
+      />
+      {/* Inner cursor */}
+      <div
+        className="fixed top-0 left-0 w-3 h-3 rounded-full bg-black pointer-events-none z-[9999] transition-transform duration-75 ease-out"
+        style={{
+          transform: `translate(${position.x - 6}px, ${position.y - 6}px)`,
+        }}
+      />
+    </>
+  );
 }
