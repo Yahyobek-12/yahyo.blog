c18a513d788f2739ba5efaf5212ebe70a09fedd9
Yahyobek <caisophiya@gmail.com>
2025-07-27 21:37:35 +0500

    sh sh sh

--- src/app/blog/page.tsx
+++ src/app/blog/page.tsx
@@ -2,7 +2,12 @@ import React from 'react'
 
 const Page = () => {
   return (
-    <div>Page</div>
+    <div className='w-full h-screen flex items-center justify-center py-4 px-4'>
+        <div>
+            <h1 className='font-bold text-8xl'>Yahyobek</h1>
+            <h1 className='font-bold text-9xl'>Ergashboyev</h1>
+        </div>
+    </div>
   )
 }
 
